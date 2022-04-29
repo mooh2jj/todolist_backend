f18d91899a598cbf174b51c70595778d1ffa0e2f
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-04-29 19:14:55 +0900

    TodoRepositoryTest @SpringBoot -> @DataJpaTest로 변경

--- src/test/java/com/example/todolist_prac/repository/TodoRepositoryTest.java
+++ src/test/java/com/example/todolist_prac/repository/TodoRepositoryTest.java
@@ -1,30 +1,26 @@
 package com.example.todolist_prac.repository;
 
 import com.example.todolist_prac.model.TodoEntity;
-import com.example.todolist_prac.model.TodoResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@SpringBootTest
-@Transactional
+@DataJpaTest
 class TodoRepositoryTest {
 
     @Autowired

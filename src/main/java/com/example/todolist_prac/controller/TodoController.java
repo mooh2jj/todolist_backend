1c7f96db4de6d3cbca4a1692fdf77b7374b6a6b8
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-03 10:34:36 +0900

    TodoRequest Validation 처리 적용

--- src/main/java/com/example/todolist_prac/controller/TodoController.java
+++ src/main/java/com/example/todolist_prac/controller/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -26,7 +27,7 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request) {
+    public ResponseEntity<TodoResponse> create(@RequestBody @Valid TodoRequest request) {
         log.info("Create");
 
         TodoResponse todoResponse = todoService.add(request);
--- src/main/java/com/example/todolist_prac/exception/TodoExceptionHandler.java
+++ src/main/java/com/example/todolist_prac/exception/TodoExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.example.todolist_prac.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.example.todolist_prac.exception.ErrorCode.INTERNAL_SERVER_ERROR;
 
@@ -66,5 +71,23 @@ public class TodoExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //BindingResult Validation 처리
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        log.error("MethodArgumentNotValidException: ", ex);
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }

7a8a39afd60669f6c82c302a438a21a386df3e21
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-14 03:08:11 +0900

    PlainTodoJobConfig - reader, writer 구현 성공

--- src/main/java/com/example/todolist_prac/repository/TodoRepository.java
+++ src/main/java/com/example/todolist_prac/repository/TodoRepository.java
@@ -1,7 +1,10 @@
 package com.example.todolist_prac.repository;
 
 import com.example.todolist_prac.model.TodoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+    Page<TodoEntity> findBy(Pageable pageable);
 }
